a6ebc304b08e19d4054580eb81b07a9fad7724bd
George James <george.okpe.james@outlook.com>
2020-07-07 08:58:28 +0400

    Remove watermark from video

--- react/features/large-video/components/LargeVideo.web.js
+++ react/features/large-video/components/LargeVideo.web.js
@@ -68,8 +68,6 @@ class LargeVideo extends Component<Props> {
                 </div>
                 <div id = 'etherpad' />
 
-                <Watermarks />
-
                 <div id = 'dominantSpeaker'>
                     <div className = 'dynamic-shadow' />
                     <div id = 'dominantSpeakerAvatarContainer' />
