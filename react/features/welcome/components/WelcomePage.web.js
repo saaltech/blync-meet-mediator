fec6c0c01a4fbbb0d684468f53a8a5aaf41d9ce7
Anuj Arora <anuj@saal.ai>
2021-01-04 12:08:12 +0530

    css background changes

--- react/features/welcome/components/WelcomePage.web.js
+++ react/features/welcome/components/WelcomePage.web.js
@@ -559,7 +559,7 @@ class WelcomePage extends AbstractWelcomePage {
                     && this._renderInsecureRoomNameWarning(this._roomInputRef.value)
                 }
             </div>
-            <div className={`${(switchActiveIndex === 1 && (_isGoogleSigninUser)) ? '' : 'contacts-placeholder'}`} >
+            <div className={`${(switchActiveIndex === 0) ? 'contacts-placeholder' : ''}`} >
                 {
                     !_isUserSignedOut && _isGoogleSigninUser && switchActiveIndex === 1 ? <CalendarProfile /> : <> </>
                 }
